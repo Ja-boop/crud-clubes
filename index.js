800430e0c39f0ba68f6d9714e5c8b4d73362e76d
Ja-boop <agusmisiones@gmail.com>
2020-10-13 07:04:09 -0300

    Inicio

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const multer = require('multer');
 
 const upload = multer({ dest: './uploads/imagenes'  });
 const exphbs = require('express-handlebars');
+const { json } = require('express');
 
 const PUERTO = 8080;
 const app = express();
@@ -13,20 +14,43 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.static(`${__dirname}/uploads`));
+app.use(express.static('public'));
 
 filenames = fs.readdirSync('./data/equipos');
+
+let rawdata = fs.readFileSync('./data/equipos.json');
+let equiposJSON = JSON.parse(rawdata);
+
+let nombreEquipos = equiposJSON.map((equipos) => {
+  return equipos.name;
+});
+
+let paisEquipos = equiposJSON.map((equipos) => {
+  return equipos.area.name;
+});
+
+let idEquipos = equiposJSON.map((equipos) => {
+  return equipos.id;
+});
 
 app.get('/', (req, res) => {
     res.render('home', {
       layout: 'main',
       data: {
         cantidadEquipos: filenames.length,
+        nombreEquipos,
+        paisEquipos,
+        idEquipos,
       },
     });
   });
+
+app.get('/equipos/agregar', (req, res) => {
+  res.render()
+});
   
   
 
-  app.listen(PUERTO);
-  console.log(`Escuchando en http://localhost:${PUERTO}`);
+app.listen(PUERTO);
+console.log(`Escuchando en http://localhost:${PUERTO}`);
   
