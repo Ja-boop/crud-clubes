bb727672e8f196002ddf51b9df80a26c94f6437b
Ja-boop <agusmisiones@gmail.com>
2020-10-14 15:53:45 -0300

    Routes, 404 handler

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const fs = require('fs');
 const express = require('express');
+const app = express();
 const multer = require('multer');
 
 const upload = multer({ dest: './uploads/imagenes'  });
 const exphbs = require('express-handlebars');
 const { json } = require('express');
 
-const PUERTO = 8080;
-const app = express();
 const hbs = exphbs.create();
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.static(`${__dirname}/uploads`));
-app.use(express.static('public'));
+const PUERTO = 8080;
+app.listen(PUERTO);
+console.log(`Escuchando en http://localhost:${PUERTO}`);
 
+// Middleware
+app.use(express.urlencoded({extended: false}));
 filenames = fs.readdirSync('./data/equipos');
+
+// Routes
+app.use(require('./routes/index.js'));
+
+// Static
+app.use(express.static(`${__dirname}/uploads`));
+app.use(express.static(`${__dirname}/public`));
+
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).send('404 Not found');
+});
 
 let rawdata = fs.readFileSync('./data/equipos.json');
 let equiposJSON = JSON.parse(rawdata);
@@ -56,6 +70,5 @@ app.get('/equipos/agregar', (req, res) => {
   
   
 
-app.listen(PUERTO);
-console.log(`Escuchando en http://localhost:${PUERTO}`);
+
   
